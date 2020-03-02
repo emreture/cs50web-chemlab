9b8fa9c8c33ae6e294a2d2fdb373430edf229c67
M. Emre TÜRE <emreture@gmail.com>
2020-03-02 11:34:43 +0300

    Edit verbose_names for display labels and change blank property of users for not being required in admin site.

--- customers/models.py
+++ customers/models.py
@@ -7,9 +7,9 @@ class Customer(models.Model):
     """Laboratory customers are legal entities not individuals.
     Field 'name' represents the organization name.
     """
-    name = models.CharField(max_length=64)
-    address = models.TextField()
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="organization")
+    name = models.CharField(max_length=64, verbose_name="Company name")
+    address = models.TextField(verbose_name="Company address")
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="organization", blank=True)
 
     def __str__(self):
         return f"{self.name}"
