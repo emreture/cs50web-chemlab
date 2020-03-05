319fa28de8ae0682dadab8412569ae99f1c023a3
M. Emre TÜRE <emreture@gmail.com>
2020-03-05 09:50:54 +0300

    Create samples app models and model tests.

--- samples/models.py
+++ samples/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from customers.models import Customer
 
 # Create your models here.
+
+
+class TestMethod(models.Model):
+    number = models.CharField(max_length=32)
+    name = models.CharField(max_length=64)
+    unit = models.CharField(max_length=16)
+
+    def __str__(self):
+        return f"{self.number} {self.name}"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=16)
+    tests = models.ManyToManyField(TestMethod, related_name="products")
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Sample(models.Model):
+    number = models.IntegerField()
+    receipt_date = models.DateTimeField()
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING, related_name="samples")
+    sampling_date = models.DateTimeField(blank=True, null=True)
+    sampling_point = models.CharField(max_length=64, blank=True)
+    report_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.number} {self.product} {self.customer}"
+
+
+class Result(models.Model):
+    sample = models.ForeignKey(Sample, on_delete=models.DO_NOTHING, related_name="results")
+    test_method = models.ForeignKey(TestMethod, on_delete=models.DO_NOTHING)
+    result = models.CharField(max_length=16)
+
+    def __str__(self):
+        return f"{self.sample.number} {self.test_method.name} {self.result} {self.test_method.unit}"
--- samples/tests.py
+++ samples/tests.py
@@ -1,3 +1,71 @@
+from datetime import datetime, timezone
 from django.test import TestCase
-
+from .models import *
 # Create your tests here.
+
+
+class ModelsTestCase(TestCase):
+    def setUp(self) -> None:
+        density = TestMethod.objects.create(number="ASTM D4052", name="Density", unit="g/cm³")
+        viscosity = TestMethod.objects.create(number="ASTM D445", name="K. Viscosity", unit="mm²/s")
+        sulfur_content = TestMethod.objects.create(number="ASTM D2622", name="Sulfur content", unit="ppm")
+        flash_point = TestMethod.objects.create(number="ASTM D93", name="Flash point", unit="°C")
+        mon = TestMethod.objects.create(number="ASTM D2700", name="Motor octane number", unit="")
+
+        gasoil = Product.objects.create(name="Gasoil")
+        gasoline = Product.objects.create(name="Gasoline")
+        gasoil.tests.add(density)
+        gasoil.tests.add(viscosity)
+        gasoil.tests.add(sulfur_content)
+        gasoil.tests.add(flash_point)
+        gasoline.tests.add(density)
+        gasoline.tests.add(sulfur_content)
+        gasoline.tests.add(mon)
+
+        astor = Customer.objects.create(name="Astor A.Ş.", address="Planet Earth")
+        emek = Customer.objects.create(name="Emek A.Ş.", address="Planet Mars")
+
+        sample1 = Sample.objects.create(number=200001, receipt_date=datetime(2020, 1, 10, 9, 30, tzinfo=timezone.utc),
+                                        product=gasoil, customer=astor,
+                                        sampling_date=datetime(2020, 1, 5, tzinfo=timezone.utc),
+                                        sampling_point="Shore tank 01",
+                                        report_date=datetime(2020, 1, 15, tzinfo=timezone.utc))
+
+        sample2 = Sample.objects.create(number=200002, receipt_date=datetime(2020, 1, 10, 9, 30, tzinfo=timezone.utc),
+                                        product=gasoline, customer=astor,
+                                        sampling_date=datetime(2020, 1, 5, tzinfo=timezone.utc),
+                                        sampling_point="Shore tank 02",
+                                        report_date=datetime(2020, 1, 15, tzinfo=timezone.utc))
+
+        result1 = Result.objects.create(sample=sample1, test_method=flash_point, result="60.0")
+
+    def test_object_names(self):
+        gasoil = Product.objects.get(name="Gasoil")
+        expected_product_name = gasoil.name
+        self.assertEqual(str(gasoil), expected_product_name)
+        test_method = gasoil.tests.get(name="Density")
+        expected_test_method_name = f"{test_method.number} {test_method.name}"
+        self.assertEqual(str(test_method), expected_test_method_name)
+        sample = Sample.objects.get(number=200001)
+        expected_sample_name = f"{sample.number} {sample.product} {sample.customer}"
+        self.assertEqual(str(sample), expected_sample_name)
+        result = Result.objects.get(pk=1)
+        expected_result_name = f"{result.sample.number} {result.test_method.name} {result.result} {result.test_method.unit}"
+        self.assertEqual(str(result), expected_result_name)
+
+    def test_object_counts(self):
+        test_methods = TestMethod.objects
+        self.assertEqual(test_methods.count(), 5)
+        products = Product.objects
+        self.assertEqual(products.count(), 2)
+        gasoil_test_methods = Product.objects.get(name="Gasoil").tests
+        self.assertEqual(gasoil_test_methods.count(), 4)
+        gasoline_test_methods = Product.objects.get(name="Gasoline").tests
+        self.assertEqual(gasoline_test_methods.count(), 3)
+
+    def test_customer_samples_count(self):
+        astor_samples = Customer.objects.get(name="Astor A.Ş.").samples
+        self.assertEqual(astor_samples.count(), 2)
+        emek_samples = Customer.objects.get(name="Emek A.Ş.").samples
+        self.assertEqual(emek_samples.count(), 0)
+
