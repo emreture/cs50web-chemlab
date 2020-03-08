277a4a7288ec987255c3474dc0aa90dd6927fdc6
M. Emre TÜRE <emreture@gmail.com>
2020-03-08 23:13:22 +0300

    Change pagination per_page and add link to new customer form in page.

--- customers/views.py
+++ customers/views.py
@@ -38,7 +38,7 @@ def index(request):
         customers = Customer.objects.filter(name__icontains=query).order_by('id')
     else:
         customers = Customer.objects.all().order_by('id')
-    paginator = Paginator(customers, 5)
+    paginator = Paginator(customers, 10)
     if goto_last_page:
         page_number = paginator.num_pages
     page_obj = paginator.get_page(page_number)
