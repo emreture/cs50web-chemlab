29ab59a3997004ad18a742585f320d4d25803b1a
M. Emre TÜRE <emreture@gmail.com>
2020-03-08 22:37:20 +0300

    Add datetime input format for Android KitKat browser.

--- samples/forms.py
+++ samples/forms.py
@@ -3,7 +3,7 @@ from .models import Sample
 
 
 class SampleForm(forms.ModelForm):
-    receipt_date = forms.DateTimeField(input_formats=['%Y-%m-%dT%H:%M', '%Y-%m-%d %H:%M'],
+    receipt_date = forms.DateTimeField(input_formats=['%Y-%m-%dT%H:%M', '%Y-%m-%d %H:%M', '%Y-%m-%dT%H:%MZ'],
                                        widget=forms.DateTimeInput(format='%Y-%m-%dT%H:%M',
                                                                   attrs={'type': 'datetime-local',
                                                                          'class': 'form-control'}))
