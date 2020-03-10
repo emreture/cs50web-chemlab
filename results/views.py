a35acbb0d9b81cfeb734ac9215e276b8f08bd451
M. Emre TÜRE <emreture@gmail.com>
2020-03-10 16:52:51 +0300

    Complete template and view of results page.

--- results/views.py
+++ results/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.http import HttpResponseNotFound, Http404
-from samples.models import Sample
-from django.conf import settings
+from django.contrib import messages
+from django.http import Http404
+from samples.models import Sample, Result, TestMethod
 
 # Create your views here.
 
@@ -14,7 +14,6 @@ def index(request):
     page_number = request.GET.get('page')
     user = request.user
     organization = user.organization.all()
-    print(organization)
     if query:
         if user.is_staff:
             samples = Sample.objects.filter(number__icontains=query).order_by('-id')
@@ -44,22 +43,61 @@ def index(request):
     page_obj = paginator.get_page(page_number)
     context = {
         'page_obj': page_obj,
-        'query': query,
+        'query': query
     }
     return render(request, "results/index.html", context)
 
 
 @login_required(login_url="login")
 def results(request, sample_id):
-    user = request.user
     sample = get_object_or_404(Sample, pk=sample_id)
+    user = request.user
     if not user.is_staff:
         organization = user.organization.all()
         if sample.customer not in list(organization):
             raise Http404(f"User not allowed to access analysis reports of {sample.customer}.")
-    print(sample.results.all())
-    form = None
+    if request.method == "POST":
+        sample.results.all().delete()
+        error = False
+        for key, item in request.POST.items():
+            if key.startswith("test_") and item != "":
+                test_id = key.split("test_")[-1]
+                test_result = item
+                try:
+                    test_method = TestMethod.objects.get(pk=test_id)
+                    r = Result.objects.create(sample=sample, test_method=test_method, result=test_result)
+                    sample.results.add(r)
+                except TestMethod.DoesNotExist:
+                    error = True
+        if error:
+            msg = "Some results were not updated."
+            messages.add_message(request, messages.ERROR, msg)
+        else:
+            msg = f"<b>{sample}</b> results were updated successfully."
+            messages.add_message(request, messages.SUCCESS, msg)
+    result_list = list()
+    for i in sample.product.tests.all():
+        result_dict = dict()
+        result_dict['id'] = i.id
+        result_dict['number'] = i.number
+        result_dict['name'] = i.name
+        try:
+            result_dict['result'] = sample.results.get(test_method=i.id).result
+        except Result.DoesNotExist:
+            result_dict['result'] = ""
+        result_dict['unit'] = i.unit
+        if user.is_staff:
+            result_list.append(result_dict)
+        else:
+            if result_dict['result']:
+                result_list.append(result_dict)
+    if user.is_staff:
+        readonly = ""
+    else:
+        readonly = "readonly"
     context = {
-        'form': form,
+        'results': result_list,
+        'sample': sample,
+        'readonly': readonly
     }
     return render(request, "results/details.html", context)
