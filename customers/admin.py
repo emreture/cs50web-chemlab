b742013fbbdbeed966f9963f5a2b0f47dbd41a9d
M. Emre TÜRE <emreture@gmail.com>
2020-03-11 13:21:52 +0300

    Show users of customer in admin panel.

--- customers/admin.py
+++ customers/admin.py
@@ -4,4 +4,17 @@ from .models import Customer
 # Register your models here.
 
 
-admin.site.register(Customer)
+# admin.site.register(Customer)
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["name", "get_users"]
+    list_filter = ["users"]
+
+    def get_users(self, obj):
+        return list(obj.users.all())
+    get_users.short_description = "Users"
+
+    class Meta:
+        model = Customer
